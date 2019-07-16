89819c44df8a493a6f868b858a519140da2579d2
zjulien <zinet.julien@hotmail.com>
2019-07-16 14:55:37 +0000

    création de la page des evenements + le controller et requête SQL + css sur la page login

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,30 +2,113 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Entity\Event;
+use App\Entity\Season;
+use App\Services\UserLogged;
 use App\Application\Controller;
 use App\Application\DatabaseConfig;
-use App\Entity\Season;
-use App\Entity\User;
-use App\Services\UserLogged;
 
 class EventController extends Controller
 {
-    public function list(){
+    public function list()
+    {
 
-    if (!UserLogged::isLogged()) {
-        header('Location: /');
-        return;
-    }
-    $event = new Event ();
-    $events = $event->getAllEvent();
-    return $this->twig->render(
-        'event/event.html.twig',
-        [
-            'events' => $events
-        ]
+        if (!UserLogged::isLogged()) {
+            header('Location: /');
+            return;
+        }
+        $event = new Event();
+        $events = $event->getAllEvent();
+        return $this->twig->render(
+            'event/list.html.twig',
+            [
+                'events' => $events
+            ]
 
         );
+    }
+    public function add()
+    {
+        if (!UserLogged::isLogged()) {
+            header('Location: /');
+            return;
+        }
 
-}
+        if (isset($_POST['form_event'])) {
 
+            $season = new season();
+            $seasons = $season->getAllSeason();
+            $event = new Event();
+            $reponseValidityForm = $this->validForm();
+            if (gettype($reponseValidityForm) === 'boolean') {
+                $event->add(
+                    $_POST['date'],
+                    $_POST['lieu'],
+                    $_POST['distance'],
+                    $_POST['nrb_enfants'],
+                    $_POST['saison_id'],
+                    $_POST['saisons']
+                    
+                );
+
+                header('Location: /dashboard/event');
+                return;
+            }
+
+            return $this->twig->render(
+                'event/form.html.twig',
+                [
+                    'error' => $reponseValidityForm,
+                    'values' => $_POST,
+                     'saisons' => $seasons
+                ]
+
+                );
+
+            return $this->twig->render(
+                'event/form.html.twig'
+
+            );
+
+
+        }
+
+        return $this->twig->render(
+            'event/form.html.twig'
+        );
+    }
+
+    private function validForm()
+    {
+        $date = $_POST['date'];
+        $lieu = $_POST['lieu'];
+        $distance = $_POST['distance'];
+        $nrb_enfants = $_POST['enfants'];
+
+       
+
+        if (strlen(trim($date)) === 0) {
+            $error['date'] = true;
+        }
+        if (strlen(trim($lieu)) === 0) {
+            $error['lieu'] = true;
+        }
+
+
+
+        if (strlen(trim($distance)) === 0) {
+            $error['distance'] = true;
+        }
+
+        if (strlen(trim($nrb_enfants)) === 0) {
+            $error['enfants'] = true;
+        }
+
+        if (count($error) !== 0) {
+            return $error;
+        }
+
+        return true;
+    }
 }
--- src/entity/Event.php
+++ src/entity/Event.php
@@ -1,14 +1,41 @@
- <?php
+<?php
 
-// namespace App\Entity;
+namespace App\Entity;
 
-// use App\Application\Database;
+use App\Application\Database;
 
-// class Event extends Database
-// {
+class Event extends Database
+{
 
+    public function getAllEvent()
+    {
+        $sql = 'SELECT id, date, distance, lieu, saison_id, nrb_enfants FROM event ';
+        $this->prepare($sql);
+        $this->execute();
 
+        return $this->fetchAll();
+    }
 
+public function add($date, $distance, $lieu, $saison_id, $nrb_enfants){
 
-//     public function list()
-// } #}
+        $sql='INSERT INTO event(date, distance, lieu, distance, saison_id, nrb_enfants) values(:date, :distance, :lieu, :saison_id, :nrb_enfants)';
+        $this-prepare($sql);
+        $this->bindParam(':date', $date, \PDO::PARAM_STR);
+        $this->bindParam(':lieu', $lieu, \PDO::PARAM_STR);
+        $this->bindParam(':distance', $distance, \PDO::PARAM_STR);
+        $this->bindParam(':saison_id' , $saison_id, \PDO::PARAM_STR);
+        $this->bindParam('nrb_enfants', $nrb_enfants, \PDO::PARAM_INT);
+        $this->execute();
+        
+
+}
+
+public function getAllSeason()
+    {
+        $sql = "select id, nom_saison from saison";
+        $this->prepare($sql);
+        $this->execute();
+        return $this->fetchAll();
+    }
+
+} 
